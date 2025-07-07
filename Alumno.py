7b00bd32453eb595eceaf3f50f0df599267266c0
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-07-06 21:42:00 -0400

    Crear clases Alumno

---
+++ Alumno.py
@@ -0,0 +1,73 @@
+from abc import ABC, abstractmethod
+
+
+class IAlumno(ABC):
+    def __init__(self, nombre, edad, rut):
+        self.nombre = nombre
+        self.edad = edad
+        self.rut = rut
+
+
+# No estudia ni puede hacer ayudantias
+class Alumni(IAlumno):
+    pass
+
+
+class IEstudiar(ABC):
+    @abstractmethod
+    def estudiar(self):
+        pass
+
+
+# Solo estudia
+class EstudianteComun(IAlumno, IEstudiar):
+    def estudiar(self):
+        print("Estudiante comun estudiando")
+
+
+class IAyudante(ABC):
+    @abstractmethod
+    def hacer_ayudantia(self):
+        pass
+
+
+# Pueden estudiar y hacer ayudantías
+class EstudianteAyudante(IAlumno, IAyudante):
+    def estudiar(self):
+        print("Estudiante ayudante estudiando")
+
+    def hacer_ayudantia(self):
+        print("Estudiante ayudante haciendo ayudantía")
+
+
+class IClases(ABC):
+    @abstractmethod
+    def hacer_clases(self):
+        pass
+
+
+# Estudia y hace clases
+class EstudianteMagister(IEstudiar, IClases):
+    def estudiar(self):
+        print("Estudiante magister estudiando")
+
+    def hacer_clases(self):
+        print("Estudiante magister haciendo clases")
+
+
+class IInvestigar(ABC):
+    @abstractmethod
+    def hacer_investigacion(self):
+        pass
+
+
+# Estudia, hace clases e investiga
+class EstudianteDoctorado(IEstudiar, IClases, IInvestigar):
+    def estudiar(self):
+        print("Estudiante doctorado estudiando")
+
+    def hacer_clases(self):
+        print("Estudiante doctorado haciendo clases")
+
+    def hacer_investigacion(self):
+        print("Estudiante doctorado haciendo clases")
